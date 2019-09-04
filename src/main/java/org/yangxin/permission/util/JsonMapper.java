8c2508838b99ea05a3f36c3484a05289d95ec571
yangxin <1393003255@qq.com>
2019-09-04 21:32:03 +0800

    feat: 新增JsonMapper类

---
+++ src/main/java/org/yangxin/permission/util/JsonMapper.java
@@ -0,0 +1,46 @@
+package org.yangxin.permission.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JsonMapper {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        // config
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setFilterProvider(new SimpleFilterProvider().setFailOnUnknownId(false));
+        // 这个地方有点问题，源码的版本有点低
+//        objectMapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_EMPTY);
+    }
+
+    public static <T> String obj2String(T src) {
+        if (src == null) {
+            return null;
+        }
+        try {
+            return src instanceof String ? (String) src : objectMapper.writeValueAsString(src);
+        } catch (Exception e) {
+            log.warn("parse object to String exception, error: [{}]", e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String src, TypeReference<T> typeReference) {
+        if (src == null || typeReference == null) {
+            return null;
+        }
+        try {
+            return (T) (typeReference.getType().equals(String.class) ? src : objectMapper.readValue(src, typeReference));
+        } catch (Exception e) {
+            log.warn("parse String to Object exception, String: [{}], TypeReference<T>: [{}]", src, typeReference.getType(), e);
+            return null;
+        }
+    }
+}
