59f52383b3f9dd4224e549e769e8a6cbc410afdf
yangxin <1393003255@qq.com>
2019-09-18 11:50:28 +0800

    feat: 完成用户登录接口与界面开发-2

---
+++ src/main/java/org/yangxin/permission/controller/UserController.java
@@ -0,0 +1,71 @@
+package org.yangxin.permission.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.yangxin.permission.model.SysUser;
+import org.yangxin.permission.service.SysUserService;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 游客Controller
+ *
+ * @author yangxin
+ * 2019/09/18 11:29
+ */
+@Controller
+public class UserController {
+    @Resource
+    private SysUserService sysUserService;
+
+    @RequestMapping("/logout.page")
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().invalidate();
+        String path = "signin.jsp";
+        response.sendRedirect(path);
+    }
+
+    /**
+     * 游客登录
+     */
+    @RequestMapping("/login.page")
+    public void login(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        SysUser sysUser = sysUserService.findByKeyword(username);
+        String errorMsg = "";
+        String ret = request.getParameter("ret");
+
+        if (StringUtils.isBlank(username)) {
+            errorMsg = "用户名不可以为空";
+        } else if (StringUtils.isBlank(password)) {
+            errorMsg = "密码不可以为空";
+        } else if (sysUser == null) {
+            errorMsg = "查询不到指定的用户";
+        } else if (sysUser.getStatus() != 1) {
+            errorMsg = "用户已被冻结，请联系管理员";
+        } else {
+            // login success
+            request.getSession().setAttribute("user", sysUser);
+            if (StringUtils.isNotBlank(ret)) {
+                response.sendRedirect(ret);
+            } else {
+                response.sendRedirect("/admin/index.page");
+            }
+        }
+
+        request.setAttribute("error", errorMsg);
+        request.setAttribute("username", username);
+        if (StringUtils.isNotBlank(ret)) {
+            request.setAttribute("ret", ret);
+        }
+        String path = "signin.jsp";
+        request.getRequestDispatcher(path).forward(request, response);
+    }
+}
